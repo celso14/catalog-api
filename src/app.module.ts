a148fd36476bb481345021059a491018c64dd33c
celso14 <celso.botelho14@gmail.com>
2023-03-11 17:21:36 -0300

    config: configurate the struct of the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ProductsModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [AppController],
 })
 export class AppModule {}
