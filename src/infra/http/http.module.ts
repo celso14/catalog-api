2e1c09895a665d76c43482c9d3f8e4cb61c8aa1f
celso14 <celso.botelho14@gmail.com>
2023-03-12 15:47:00 -0300

    feat: http layer

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from '../../app/use-cases/products.service';
-import { ProductsController } from './controllers/products.controller';
+import { CategoriesService } from 'src/app/use-cases/categories.service';
+import { DatabaseModule } from '../db/database.module';
+import { CategoriesController } from './controllers/categories.controller';
 
 @Module({
-  controllers: [ProductsController],
-  providers: [ProductsService]
+    imports: [DatabaseModule],
+    controllers: [
+        CategoriesController,
+    ],
+    providers: [
+        CategoriesService,
+    ]
 })
 export class ProductsModule {}
