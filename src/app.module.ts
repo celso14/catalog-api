f533d97d00d1befdd4232aaee82b305d4981ebb3
celso14 <celso.botelho14@gmail.com>
2023-03-12 12:03:12 -0300

    feat: category crud initialize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsModule } from './infra/http/http.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, CategoriesModule],
   controllers: [AppController],
 })
 export class AppModule {}
