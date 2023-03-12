1beaeb2c69e8a906e2f0346be11212cfd57670be
celso14 <celso.botelho14@gmail.com>
2023-03-12 15:49:55 -0300

    feat: main.ts configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { NotFoundInterceptor } from './app/common/errors/interceptors/notfound.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.use(helmet());
+
+    app.enableCors({
+      origin: '*'
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        }),
+    );
+
+    app.useGlobalInterceptors(new NotFoundInterceptor());
+
+    await app.listen(3000);
 }
 bootstrap();
